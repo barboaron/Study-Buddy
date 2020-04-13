d1120633cd1ee4139249b1dcc8619e027fe528d4
ItaiFarber <54215551+ItaiFarber@users.noreply.github.com>
2020-04-13 17:04:48 +0300

    Added email confirmation, Profile Creation and Profile Update

--- src/models/User.js
+++ src/models/User.js
@@ -25,6 +25,10 @@ const UserSchema = new Schema({
   date: {
     type: Date,
     default: Date.now
+  },
+  confirmed: {
+    type: Boolean,
+    required: true
   }
 });
 module.exports = User = mongoose.model("users", UserSchema);
